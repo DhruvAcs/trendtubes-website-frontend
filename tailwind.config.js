a38b101a961daa47d2049c14680a1797c1fdafe8
BuildTools <unconfigured@null.spigotmc.org>
2021-04-18 18:53:41 -0400

    testing fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 // const colors = require('tailwindcss/colors')
 
 module.exports = {
-	purge: [
-		'./src/components/**/*.{js,ts,jsx,tsx}',
-		'./src/pages/**/*.{js,ts,jsx,tsx}',
-	],
+	// purge: [
+	// 	'./src/components/**/*.{js,ts,jsx,tsx}',
+	// 	'./src/pages/**/*.{js,ts,jsx,tsx}',
+	// ],
 	// purge: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
 	variants: {
 		extend: {

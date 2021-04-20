1757709a9ff454a919539564237165497de61cc2
BuildTools <unconfigured@null.spigotmc.org>
2021-04-19 21:27:19 -0400

    api changes

--- utils/backend-api.ts
+++ utils/backend-api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_PREURL = 'http://localhost:3005';
+const API_PREURL = 'http://api.trendtubes.com';
 axios.defaults.withCredentials = true;
 // axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 
